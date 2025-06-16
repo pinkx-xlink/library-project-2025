aa7f78b2f29437ac4d35e1e0b8d288ec41778e25
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-06-16 16:58:49 -0400

    Write the pseudocode

---
+++ main.js
@@ -0,0 +1,21 @@
+
+// Create Book constructor
+
+// Create a second function that can take arguments
+// and create a new book from those args
+// and store the new Book in an array
+// all books should have a unique id,
+// generated using crypto.randomUUID()
+// (Challenge to self: implement local storage)
+
+
+// Write a func that loops through each Book
+// and displays it on the page on its own card
+
+
+// Add a New Book btn
+
+
+// Add a btn on each book to remove it
+
+// Add a btn on each book to change its read status
