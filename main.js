fcaec3a66f4789998dbdd762afd71ee8480cdc07
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-06-16 17:18:01 -0400

    Create Book function and myLibrary array to store Book objects in

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+const myLibrary = [];
 
 // Create Book constructor
+function Book(title, author, pages, read) {
+if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor." )
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return (`Title: ${this.title} / Author: ${this.author} / Pages: ${this.pages} / Read: ${this.read}`);
+    }
+}
 
 // Create a second function that can take arguments
 // and create a new book from those args
@@ -7,7 +20,9 @@
 // all books should have a unique id,
 // generated using crypto.randomUUID()
 // (Challenge to self: implement local storage)
+function addBookToLibrary() {
 
+}
 
 // Write a func that loops through each Book
 // and displays it on the page on its own card
